4f46a5a8ed79ccbe9dd0385aaa705b8da710b65e
Lulu <cacameur@gmx.fr>
2023-07-08 13:16:13 +0200

    Increase version to 1.0.1

--- Units/u_common.pas
+++ Units/u_common.pas
@@ -9,7 +9,7 @@ uses
 
 const
   APP_NAME = 'Give Your Voice';
-  APP_VERSION = '1.0.0';
+  APP_VERSION = '1.0.1';
 
   // GitHub
   URL_FOR_VERSION_ON_GITHUB = 'https://raw.githubusercontent.com/Lulu04/Give-Your-Voice/main/version';
